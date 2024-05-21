576c812471cfbdd58544ae431a1e1f6ae259c600
Gene Yeo <geneyeotech@gmail.com>
2024-05-21 19:23:03 +0800

    Minor UI tweaks

--- assignment1/app/cart/CartCheckout.tsx
+++ assignment1/app/cart/CartCheckout.tsx
@@ -5,12 +5,12 @@ export default function CartCheckout() {
     const { totalAmt, clearCart } = useCartStore();
     return (
         <footer className="flex flex-col lg:flex-row justify-end items-start lg:items-center bg-cyan-600">
-            <h1 className="font-medium text-4xl mx-8 sm:mr-16 my-4">
+            <h1 className="font-medium text-3xl lg:text-4xl mx-8 sm:mr-16 my-4">
                 Total: ${totalAmt.toFixed(2)}
             </h1>
             <button
                 onClick={clearCart}
-                className="text-2xl font-medium rounded-xl h-14 px-24 lg:px-10 ml-8 xl:mr-10 2xl:mr-64 mb-2 lg:my-4 bg-green-800 hover:bg-green-700"
+                className="text-2xl font-medium rounded-xl px-40 lg:px-10 py-2 lg:py-4 ml-8 mr-12 2xl:mr-64 mb-2 lg:my-4 bg-green-800 hover:bg-green-700"
             >
                 Checkout
             </button>
